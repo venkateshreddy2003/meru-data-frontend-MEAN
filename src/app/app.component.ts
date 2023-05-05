06bc6e9be77ae5bbe4c84fa56b3cc148b8272e18
venkateshreddy2003 <20bq1a05j4@vvit.net>
2023-05-05 10:30:29 +0530

    modified time from 24hr to 12 hr

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -63,12 +63,44 @@ export class AppComponent implements OnInit {
           d.setMinutes(endTime[1]);
           return d > new Date();
         });
+        open.forEach((x: any) => {
+          const endTime = x.endTime.split(':');
+          const startTime = x.startTime.split(':');
+          let hours = endTime[0]; // gives the value in 24 hours format
+          let AmOrPm = hours >= 12 ? 'pm' : 'am';
+          hours = hours % 12 || 12;
+          let minutes = endTime[1];
+          let finalTime = hours + ':' + minutes + ' ' + AmOrPm;
+          x.endTime = finalTime;
+          hours = startTime[0]; // gives the value in 24 hours format
+          AmOrPm = hours >= 12 ? 'pm' : 'am';
+          hours = hours % 12 || 12;
+          minutes = startTime[1];
+          finalTime = +hours + ':' + minutes + ' ' + AmOrPm;
+          x.startTime = finalTime;
+        });
         const closed = lst.filter((x: any) => {
           const d = new Date(x.endDate);
           const endTime = x.endTime.split(':');
           d.setHours(endTime[0]);
           d.setMinutes(endTime[1]);
           return d <= new Date();
+        });
+        closed.forEach((x: any) => {
+          const endTime = x.endTime.split(':');
+          const startTime = x.startTime.split(':');
+          let hours = endTime[0]; // gives the value in 24 hours format
+          let AmOrPm = hours >= 12 ? 'pm' : 'am';
+          hours = hours % 12 || 12;
+          let minutes = endTime[1];
+          let finalTime = +hours + ':' + minutes + ' ' + AmOrPm;
+          x.endTime = finalTime;
+          hours = startTime[0]; // gives the value in 24 hours format
+          AmOrPm = hours >= 12 ? 'pm' : 'am';
+          hours = hours % 12 || 12;
+          minutes = startTime[1];
+          finalTime = +hours + ':' + minutes + ' ' + AmOrPm;
+          x.startTime = finalTime;
         });
         console.log(open);
         console.log('from app comnponent' + closed);
